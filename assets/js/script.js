860da1f81ba07ef09e3c07cdd545cba0b8e08041
junedkaxioned <junedk@axioned.com>
2022-10-10 17:41:54 +0530

    js completed part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-/* Author: Zishan Ansari */
+var accordions = document.querySelectorAll('.accordion');
+var accordionContents = document.querySelectorAll('.accordion-content');
+var tabs = document.querySelectorAll('.tab');
+var tabContents = document.querySelectorAll('.tab-content');
+
+// function for both (tab & Accordions)
+function tabFunction(list, content) {
+  list.forEach(function (item, idx) {
+    // Events on Tab & accordion
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      for (var i = 0; i < list.length; i++) {
+        list[i].classList.remove('active');
+        content[i].classList.remove('active')
+      }    
+      this.classList.add('active');
+      content[idx].classList.add('active')
+    })
+  })
+}
+
+tabFunction(accordions, accordionContents);
+tabFunction(tabs, tabContents);
